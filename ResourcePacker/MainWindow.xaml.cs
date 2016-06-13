39d921d987769b266fb75f416114c0f53c831cef
Cesar Gonzalez <cegonse@gmail.com>
2016-06-13 19:29:21 +0200

    Package manager advances

--- ResourcePacker/MainWindow.xaml.cs
+++ ResourcePacker/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace ResourcePacker
 {
@@ -20,6 +21,8 @@ namespace ResourcePacker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PackageManager _packer;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +30,78 @@ namespace ResourcePacker
 
         private void folderButton_Click(object sender, RoutedEventArgs e)
         {
+            FolderBrowserDialog fbDlg = new FolderBrowserDialog();
+            DialogResult dlgRes = fbDlg.ShowDialog();
 
+            if (dlgRes == System.Windows.Forms.DialogResult.OK)
+            {
+                string targetFolder = fbDlg.SelectedPath;
+                _packer = new PackageManager(targetFolder);
+                RefreshFileList();
+            }
+        }
+
+        public void RefreshFileList()
+        {
+            List<ResourceEntry> ent = _packer.GetFilesInDirectory();
+            filesListBox.Items.Clear();
+
+            foreach (ResourceEntry et in ent)
+            {
+                filesListBox.Items.Add(et.Filename);
+            }
+        }
+
+        private void filesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (filesListBox.SelectedIndex != -1)
+            {
+                List<ResourceEntry> ent = _packer.GetFilesInDirectory();
+                int i = filesListBox.SelectedIndex;
+
+                nameLabel.Content = "Name: " + ent[i].Filename;
+                sizeLabel.Content = "Length: " + ent[i].Length.ToString() + " bytes";
+                offsetLabel.Content = "Offset: " + ent[i].Offset.ToString() + " bytes";
+
+                typeLabel.Content = "Type: ";
+
+                warningImage.Visibility = Visibility.Hidden;
+                warnFormatLabel.Visibility = Visibility.Hidden;
+
+                switch (ent[i].Type)
+                {
+                    case ResourceType.Binary:
+                        typeLabel.Content += "Generic binary";
+                        break;
+
+                    case ResourceType.AudioPCM:
+                        typeLabel.Content += "PCM (Uncompressed) audio";
+                        break;
+
+                    case ResourceType.AudioVorbis:
+                        typeLabel.Content += "Ogg Vorbis (Compressed) audio";
+                        break;
+
+                    case ResourceType.MeshAnimatedSkeletal:
+                        typeLabel.Content += "Skeleton / animated mesh (Autodesk FBX)";
+                        break;
+
+                    case ResourceType.MeshStatic:
+                        typeLabel.Content += "Static mesh (Wavefront OBJ)";
+                        break;
+
+                    case ResourceType.TexturePvr:
+                        typeLabel.Content += "Dreamcast compressed texture (PVRTC)";
+                        warningImage.Visibility = Visibility.Visible;
+                        warnFormatLabel.Visibility = Visibility.Visible;
+                        warnFormatLabel.Content = "PVRTC Textures are not supported yet";
+                        break;
+
+                    case ResourceType.TextureRaw:
+                        typeLabel.Content += "RAW (Uncompressed) texture";
+                        break;
+                }
+            }
         }
     }
 }
---
+++ ResourcePacker/PackageManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourcePacker
+{
+    public struct ResourceEntry
+    {
+        public ResourceType Type;
+        public string Filename;
+        public string CompleteFilename;
+        public UInt32 Length;
+        public UInt32 Offset;
+    }
+
+    public enum ResourceType : byte
+    {
+        Binary = 0x00,
+        TextureRaw,
+        TexturePvr,
+        MeshStatic,
+        MeshAnimatedSkeletal,
+        AudioPCM,
+        AudioVorbis
+    }
+
+    public enum ResourceVersion
+    {
+        Uncompressed = 0x00,
+        ZLIB
+    }
+
+    public class PackageManager
+    {
+        private string _directory;
+        private List<ResourceEntry> _fileList;
+
+        public PackageManager(string directory)
+        {
+            _directory = directory;
+            _fileList = new List<ResourceEntry>();
+
+            RefreshFileList();
+        }
+
+        public void RefreshFileList()
+        {
+            // Get all the files in the directory
+            string[] files = Directory.GetFiles(_directory);
+
+            // For each file: fetch its size, type and
+            // calculate its offset in the package
+            UInt32 currentOffset = 0;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo fInfo = new FileInfo(files[i]);
+                ResourceEntry ent = new ResourceEntry();
+                string ext = fInfo.Extension.ToLower();
+
+                if (ext.Contains("png") || ext.Contains("jpg") || ext.Contains("jpeg") || ext.Contains("bmp") ||
+                    ext.Contains("tif") || ext.Contains("tiff") || ext.Contains("gif"))
+                {
+                    ent.Type = ResourceType.TextureRaw;
+                }
+                else if (ext.Contains("pvr"))
+                {
+                    ent.Type = ResourceType.TexturePvr;
+                }
+                else if (ext.Contains("ogg"))
+                {
+                    ent.Type = ResourceType.AudioVorbis;
+                }
+                else if (ext.Contains("wav"))
+                {
+                    ent.Type = ResourceType.AudioPCM;
+                }
+                else if (ext.Contains("obj"))
+                {
+                    ent.Type = ResourceType.MeshStatic;
+                }
+                else if (ext.Contains("fbx"))
+                {
+                    ent.Type = ResourceType.MeshAnimatedSkeletal;
+                }
+                else
+                {
+                    ent.Type = ResourceType.Binary;
+                }
+
+                ent.Filename = fInfo.Name.Split('.')[0];
+                ent.CompleteFilename = fInfo.FullName;
+                ent.Length = (UInt32)fInfo.Length;
+                ent.Offset = currentOffset;
+
+                _fileList.Add(ent);
+
+                currentOffset += (UInt32)fInfo.Length;
+            }
+        }
+
+        public List<ResourceEntry> GetFilesInDirectory()
+        {
+            return _fileList;
+        }
+    }
+}
